f7e2ca0841e169cecb72310058ba3e2ddfbbdf6a
Mileshaase <mileshaase.imperium@gmail.com>
2022-03-31 14:25:54 -0400

    cool

--- read.py
+++ read.py
@@ -1,9 +1,12 @@
 from english_words import english_words_lower_alpha_set
 
+words = ""
+
 with open('Script.txt','r+') as Script:
     lines = Script.read().split(" ")
     for word in lines:
         if(word in english_words_lower_alpha_set):
             if(not len(word) <= 2):
-                foundWords = open("WordsFound.txt", "a")
-                foundWords.write(f"{word}\n")
+                words = words + (f"{word}\n")
+    foundWords = open("WordsFound.txt", "w")
+    foundWords.write(words)
